d3e3801065fec15f61920242b30c919c12ebbec0
sandromarceddu <152404263+sandromarceddu@users.noreply.github.com>
2025-11-05 20:40:45 +0100

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,33 @@
+export const translations = {
+  it: {
+    menu: {
+      progetto: "Progetto",
+      prodotti: "Prodotti",
+      territori: "Territori",
+      verifica: "Verifica Origine",
+      contatti: "Contatti",
+    },
+    hero: {
+      titolo: "La blockchain al servizio dell’autenticità alimentare italiana",
+      descrizione: "Tracciabilità, trasparenza e tutela del Made in Italy.",
+      progetto: "Scopri il progetto",
+      verifica: "Verifica un prodotto"
+    }
+  },
+
+  en: {
+    menu: {
+      progetto: "Project",
+      prodotti: "Products",
+      territori: "Territories",
+      verifica: "Verify Origin",
+      contatti: "Contact",
+    },
+    hero: {
+      titolo: "Blockchain for the authenticity of Italian food",
+      descrizione: "Traceability, transparency and protection of Made in Italy.",
+      progetto: "Learn more",
+      verifica: "Verify a product"
+    }
+  }
+};
